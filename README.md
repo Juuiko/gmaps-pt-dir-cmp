0b9e1629b2e333032d58fa201fb221bbfbe10f21
Juuiko <mahonal@tcd.ie>
2021-01-25 19:53:15 +0000

    created empty flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# gmaps_pt_dir_cmp
+
+Application to search for nearby desired locations and comparing their arrival time by public transport.
---
+++ android/app/src/main/kotlin/dev/alexmahon/gmaps_pt_dir_cmp/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.alexmahon.gmaps_pt_dir_cmp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
