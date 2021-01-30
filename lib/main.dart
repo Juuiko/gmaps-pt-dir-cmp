edaa02c736dcab30b9c605e2b362a5cc9fa55a8d
Juuiko <mahonal@tcd.ie>
2021-01-30 16:14:04 +0000

    added basic map view

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-      title: Text('First Commit!'),
-    ),
-  ),
-));
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Maps',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MapView(),
+    );
+  }
+}
+
+class MapView extends StatefulWidget {
+  @override
+  _MapViewState createState() => _MapViewState();
+}
+
+class _MapViewState extends State<MapView> {
+  @override
+  Widget build(BuildContext context) {
+    // Determining the screen width & height
+    var height = MediaQuery.of(context).size.height;
+    var width = MediaQuery.of(context).size.width;
+
+    // Initial location of the Map view
+    CameraPosition _initialLocation = CameraPosition(target: LatLng(0.0, 0.0));
+
+    // For controlling the view of the Map
+    GoogleMapController mapController;
+
+
+    return Container(
+      height: height,
+      width: width,
+      child: Scaffold(
+        body: Stack(
+          children: <Widget>[
+            GoogleMap(
+              initialCameraPosition: _initialLocation,
+              myLocationEnabled: true,
+              myLocationButtonEnabled: false,
+              mapType: MapType.normal,
+              zoomGesturesEnabled: true,
+              zoomControlsEnabled: false,
+              onMapCreated: (GoogleMapController controller) {
+                mapController = controller;
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
